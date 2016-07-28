61b18341e43e35641caf13e81d617a9c42075c51
StephenT <smturvey@gmail.com>
2016-07-28 15:42:05 +0100

    new smooth scroller

--- js/scripts.js
+++ js/scripts.js
@@ -21,22 +21,15 @@
             }
         });
 
-        // SMOOTH SCROLLING
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 500);
+        
+		
+		// SMOOTH SCROLLING
+		/* smooth-scroll.js v1.3.1 */
+		(function(e,t){"use strict";var n=0,r=500,i=15,s=document.getElementsByTagName("a"),o;for(var u=0;u<s.length;u++){o=s[u].attributes.href===t?null:s[u].attributes.href.nodeValue.toString();if(o!==null&&o.length>1&&o.indexOf("#")!=-1){s[u].onclick=function(){var n,s=this.attributes.href.nodeValue.toString(),o=s.substr(0,s.indexOf("#")),u=s.substr(s.indexOf("#")+1);if(n=document.getElementById(u)){var l=(r-r%i)/i,c=f(),h=(a(n)-c)/l;if(e.history&&typeof e.history.pushState=="function")e.history.pushState({},t,o+"#"+u);for(var p=1;p<=l;p++){(function(){var t=h*p;setTimeout(function(){e.scrollTo(0,t+c)},i*p)})()}return false}}}}var a=function(e){var r=n*-1;while(e.offsetParent!=t&&e.offsetParent!=null){r+=e.offsetTop+(e.clientTop!=null?e.clientTop:0);e=e.offsetParent}return r};var f=function(){return e.pageYOffset!==t?e.pageYOffset:document.documentElement.scrollTop!==t?document.documentElement.scrollTop:document.body.scrollTop}})(window)
 
-                    //return false;
-                }
-            }
-        });
-
-        // SHOW/HIDE TRANSCRIPTS
+        
+		
+		// SHOW/HIDE TRANSCRIPTS
         $(".ou-video__transcript").hide();
         $('<p class="ou-video__toggle"><a href="#">Show transcript</a></p>').appendTo(".ou-video--transcript .ou-video__clip");
         $('p.ou-video__toggle a').click(function(){
