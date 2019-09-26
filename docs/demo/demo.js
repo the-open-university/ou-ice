3ee0f2d9f8cc0958e64c9817a2f8b934d8aefe9e
Matt Smith <matt.smith@freshegg.com>
2019-09-26 14:58:17 +0100

    Formed all files
    Updated file

--- docs/demo/demo.js
+++ docs/demo/demo.js
@@ -1,46 +1,50 @@
 // Javascripts solely for the demo site
 
-    $(document).ready(function(){
-		
-		$('.code-example').each(function() {
-		  $(this).prevAll('h2').first().append('<a class="code-example-trigger" href="#">Code</a>');
-		});
+$(document).ready(function() {
+    $(".code-example").each(function() {
+        $(this)
+            .prevAll("h2")
+            .first()
+            .append('<a class="code-example-trigger" href="#">Code</a>');
+    });
 
-        $('.code-example').each(function(example){
-            var $example = $(this);
-            var $pre = $('<pre></pre>');
-            $pre.text($example.html())
-                .addClass('prettyprint prettyprint-toglable')
-                .insertBefore($example);
-        });
+    $(".code-example").each(function(example) {
+        var $example = $(this);
+        var $pre = $("<pre></pre>");
+        $pre.text($example.html())
+            .addClass("prettyprint prettyprint-toglable")
+            .insertBefore($example);
+    });
 
-        $('.lang-html,.lang-js').each(function(key, value){
-            var $value = $(value);
-            $value.text($value.html());
-        });
-    
-        $('pre.prettyprint-toglable').hide();
-		$('.code-example-trigger').click(function () {
-		  $(this).parent().nextAll('pre.prettyprint-toglable').first().slideToggle('fast');
-		  return false;
-    });	
+    $(".lang-html,.lang-js").each(function(key, value) {
+        var $value = $(value);
+        $value.text($value.html());
+    });
 
-    $('.js-nav-toggle').click(function () {
-      $(this).toggleClass('is-active');
-      $('.js-nav').toggleClass('is-active');
-		  return false;
-    });	
-    
+    $("pre.prettyprint-toglable").hide();
+    $(".code-example-trigger").click(function() {
+        $(this)
+            .parent()
+            .nextAll("pre.prettyprint-toglable")
+            .first()
+            .slideToggle("fast");
+        return false;
+    });
+
+    $(".js-nav-toggle").click(function() {
+        $(this).toggleClass("is-active");
+        $(".js-nav").toggleClass("is-active");
+        return false;
+    });
 
     // Add button to hide side menu
 
-    $('<button class="js-nav-toggle-desktop is-active"><span class="sr-only">Open menu</span><svg xmlns="http://www.w3.org/2000/svg" width="15" height="12" class="open"><path fill="#0F4D87" fill-rule="nonzero" d="M1 2h13a1 1 0 1 0 0-2H1a1 1 0 1 0 0 2zm13 8H1a1 1 0 1 0 0 2h13a1 1 0 1 0 0-2zm0-5H1a1 1 0 1 0 0 2h13a1 1 0 1 0 0-2z"></path></svg><svg xmlns="http://www.w3.org/2000/svg" width="11" height="12" class="close"><path fill="#0F4D87" fill-rule="nonzero" d="M.21 2.1l9.2 9.2a1 1 0 0 0 1.4-1.4L1.61.7a1 1 0 0 0-1.4 1.4zM9.41.7L.21 9.9a1 1 0 0 0 1.4 1.4l9.2-9.2A1 1 0 0 0 9.41.7z"></path></svg></button>').insertAfter('.ou-ice-header__nav');
-    $('.js-nav-toggle-desktop').click(function () {
-      $(this).toggleClass('is-active');
-      $('body').toggleClass('ou-ice-nav-hidden');
-		  return false;
-    });	
-    
-		
-
-	});
+    $(
+        '<button class="js-nav-toggle-desktop is-active"><span class="sr-only">Open menu</span><svg xmlns="http://www.w3.org/2000/svg" width="15" height="12" class="open"><path fill="#0F4D87" fill-rule="nonzero" d="M1 2h13a1 1 0 1 0 0-2H1a1 1 0 1 0 0 2zm13 8H1a1 1 0 1 0 0 2h13a1 1 0 1 0 0-2zm0-5H1a1 1 0 1 0 0 2h13a1 1 0 1 0 0-2z"></path></svg><svg xmlns="http://www.w3.org/2000/svg" width="11" height="12" class="close"><path fill="#0F4D87" fill-rule="nonzero" d="M.21 2.1l9.2 9.2a1 1 0 0 0 1.4-1.4L1.61.7a1 1 0 0 0-1.4 1.4zM9.41.7L.21 9.9a1 1 0 0 0 1.4 1.4l9.2-9.2A1 1 0 0 0 9.41.7z"></path></svg></button>'
+    ).insertAfter(".ou-ice-header__nav");
+    $(".js-nav-toggle-desktop").click(function() {
+        $(this).toggleClass("is-active");
+        $("body").toggleClass("ou-ice-nav-hidden");
+        return false;
+    });
+});
--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,70 +1,137 @@
-// 
+//
 //  OU Student Theme Framework js
 //
 
 // BACK TO TOP LINK
-;(function($) {
-
-    $(document).ready(function(){
-
-        $(window).scroll(function(){
+(function($) {
+    $(document).ready(function() {
+        $(window).scroll(function() {
             // if you want to measure 1000px from the top, use --&gt; if(jQuery(window).scrollTop() &gt; 1000)
             // if you want to measure 1000px from the bottom, use  --&gt; if(jQuery(document).height() - jQuery(window).height() - jQuery(window).scrollTop() &lt; 1000)
 
             // check whether the scroll has reached 1000px from the top
-            if($(window).scrollTop() > 1000){
+            if ($(window).scrollTop() > 1000) {
                 // show back to top
-                $('#ou-site-footer .ou-to-top').stop().animate({opacity: 1});
+                $("#ou-site-footer .ou-to-top")
+                    .stop()
+                    .animate({ opacity: 1 });
             } else {
                 // hide back to top
-                $('#ou-site-footer .ou-to-top').stop().animate({opacity: 0});
+                $("#ou-site-footer .ou-to-top")
+                    .stop()
+                    .animate({ opacity: 0 });
             }
         });
 
-        
-		
-		// SMOOTH SCROLLING
-		/* smooth-scroll.js v1.3.1 */
-		(function(e,t){"use strict";var n=0,r=500,i=15,s=document.getElementsByTagName("a"),o;for(var u=0;u<s.length;u++){o=s[u].attributes.href===t?null:s[u].attributes.href.nodeValue.toString();if(o!==null&&o.length>1&&o.indexOf("#")!=-1){s[u].onclick=function(){var n,s=this.attributes.href.nodeValue.toString(),o=s.substr(0,s.indexOf("#")),u=s.substr(s.indexOf("#")+1);if(n=document.getElementById(u)){var l=(r-r%i)/i,c=f(),h=(a(n)-c)/l;if(e.history&&typeof e.history.pushState=="function")e.history.pushState({},t,o+"#"+u);for(var p=1;p<=l;p++){(function(){var t=h*p;setTimeout(function(){e.scrollTo(0,t+c)},i*p)})()}return false}}}}var a=function(e){var r=n*-1;while(e.offsetParent!=t&&e.offsetParent!=null){r+=e.offsetTop+(e.clientTop!=null?e.clientTop:0);e=e.offsetParent}return r};var f=function(){return e.pageYOffset!==t?e.pageYOffset:document.documentElement.scrollTop!==t?document.documentElement.scrollTop:document.body.scrollTop}})(window)
+        // SMOOTH SCROLLING
+        /* smooth-scroll.js v1.3.1 */
+        (function(e, t) {
+            "use strict";
+            var n = 0,
+                r = 500,
+                i = 15,
+                s = document.getElementsByTagName("a"),
+                o;
+            for (var u = 0; u < s.length; u++) {
+                o =
+                    s[u].attributes.href === t
+                        ? null
+                        : s[u].attributes.href.nodeValue.toString();
+                if (o !== null && o.length > 1 && o.indexOf("#") != -1) {
+                    s[u].onclick = function() {
+                        var n,
+                            s = this.attributes.href.nodeValue.toString(),
+                            o = s.substr(0, s.indexOf("#")),
+                            u = s.substr(s.indexOf("#") + 1);
+                        if ((n = document.getElementById(u))) {
+                            var l = (r - (r % i)) / i,
+                                c = f(),
+                                h = (a(n) - c) / l;
+                            if (
+                                e.history &&
+                                typeof e.history.pushState == "function"
+                            )
+                                e.history.pushState({}, t, o + "#" + u);
+                            for (var p = 1; p <= l; p++) {
+                                (function() {
+                                    var t = h * p;
+                                    setTimeout(function() {
+                                        e.scrollTo(0, t + c);
+                                    }, i * p);
+                                })();
+                            }
+                            return false;
+                        }
+                    };
+                }
+            }
+            var a = function(e) {
+                var r = n * -1;
+                while (e.offsetParent != t && e.offsetParent != null) {
+                    r += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+                    e = e.offsetParent;
+                }
+                return r;
+            };
+            var f = function() {
+                return e.pageYOffset !== t
+                    ? e.pageYOffset
+                    : document.documentElement.scrollTop !== t
+                    ? document.documentElement.scrollTop
+                    : document.body.scrollTop;
+            };
+        })(window);
 
-        
-		
-		// SHOW/HIDE TRANSCRIPTS
+        // SHOW/HIDE TRANSCRIPTS
         $(".ou-video__transcript").hide();
-        $('<p class="ou-video__toggle"><a href="#">Show transcript</a></p>').appendTo(".ou-video--transcript .ou-video__clip");
-        $('p.ou-video__toggle a').click(function(){
-            $(this).text($(this).text()=='Show transcript'?'Hide transcript':'Show transcript');
-            $(this).parent().parent().next().toggle();return false;
-            $(this).html(text)
-        })
-
+        $(
+            '<p class="ou-video__toggle"><a href="#">Show transcript</a></p>'
+        ).appendTo(".ou-video--transcript .ou-video__clip");
+        $("p.ou-video__toggle a").click(function() {
+            $(this).text(
+                $(this).text() == "Show transcript"
+                    ? "Hide transcript"
+                    : "Show transcript"
+            );
+            $(this)
+                .parent()
+                .parent()
+                .next()
+                .toggle();
+            return false;
+            $(this).html(text);
+        });
 
         // TOGGLE
         $(".ou-js-hidden").hide();
-        $(".ou-js-toggle").addClass('ou-toggle--closed');
-        $(".ou-js-toggle").click(function(){
-            $(this).next(".ou-js-hidden").slideToggle();
-            $(this).toggleClass('ou-toggle--open ou-toggle--closed');
+        $(".ou-js-toggle").addClass("ou-toggle--closed");
+        $(".ou-js-toggle").click(function() {
+            $(this)
+                .next(".ou-js-hidden")
+                .slideToggle();
+            $(this).toggleClass("ou-toggle--open ou-toggle--closed");
             return false;
         });
-
 
         // SHOW/HIDE MOBILE MENU
-        $(".ou-mobile-menu-toggle").click(function(){
+        $(".ou-mobile-menu-toggle").click(function() {
             $("#ou-service-links").toggleClass("visible");
-            $('body').toggleClass('ou-service-links-open');
-            $('a.ou-mobile-menu-toggle').toggleClass('icon-down icon-up')
+            $("body").toggleClass("ou-service-links-open");
+            $("a.ou-mobile-menu-toggle").toggleClass("icon-down icon-up");
             return false;
         });
 
-
         // ACCORDION
-        function openFirstPanel(){
-           $('.ou-accordion > dt:first-child').addClass('active');
-           $('.ou-accordion > dt:first-child').next().addClass('active');
+        function openFirstPanel() {
+            $(".ou-accordion > dt:first-child").addClass("active");
+            $(".ou-accordion > dt:first-child")
+                .next()
+                .addClass("active");
 
-           $('.ou-accordion.hide-all > dt:first-child').removeClass('active');
-           $('.ou-accordion.hide-all > dt:first-child').next().removeClass('active');
+            $(".ou-accordion.hide-all > dt:first-child").removeClass("active");
+            $(".ou-accordion.hide-all > dt:first-child")
+                .next()
+                .removeClass("active");
         }
 
         //var allPanels = $('.ou-accordion > dd');
@@ -72,30 +139,28 @@
 
         openFirstPanel();
 
-        $('.ou-accordion > dt > a').click(function() {
+        $(".ou-accordion > dt > a").click(function() {
             $this = $(this);
-            $target =  $this.parent().next();
+            $target = $this.parent().next();
             $dt = $this.parent();
             $container = $this.parent().parent();
-            $allPans = $this.parent().siblings('dd');
-            $allTitles = $this.parent().siblings('dt');
+            $allPans = $this.parent().siblings("dd");
+            $allTitles = $this.parent().siblings("dt");
 
-            if($target.hasClass('active')) {
-                $target.removeClass('active');
-                $dt.removeClass('active');
-            } else if($container.hasClass('allow-reveal-all')) {
-                $target.addClass('active');
-                $dt.addClass('active');
+            if ($target.hasClass("active")) {
+                $target.removeClass("active");
+                $dt.removeClass("active");
+            } else if ($container.hasClass("allow-reveal-all")) {
+                $target.addClass("active");
+                $dt.addClass("active");
             } else {
-              $allTitles.removeClass('active');
-              $allPans.removeClass('active');
-              $target.addClass('active');
-              $dt.addClass('active');
+                $allTitles.removeClass("active");
+                $allPans.removeClass("active");
+                $target.addClass("active");
+                $dt.addClass("active");
             }
 
             return false;
         });
-
     });
-
 })(jQuery);
